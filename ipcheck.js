2cd02a7acfa14267d8f680aedbfae1772c525584
xixihh <68039773+haiyang5003@users.noreply.github.com>
2022-05-22 15:10:50 +0800

    Update ipcheck.js

--- ipcheck.js
+++ ipcheck.js
@@ -1,15 +1,3 @@
-/**
-* 感谢@congcong大佬提供的js，原文件地址：https://github.com/congcong0806/surge-list/blob/master/Script/ipcheck.js
-* 用法
-* [Panel]
-* #节点检测
-* ipcheck = script-name=ipcheck, title="节点详情", content="请刷新", style=info, update-interval=1
-* ...
-* [Script]
-* #节点检测
-* ipcheck = type=generic,timeout=3,script-path=https://raw.githubusercontent.com/LucaLin233/Luca_Conf/main/Surge/JS/ipcheck.js
-*/
-
 let url = "http://ip-api.com/json/?lang=zh-CN"
 
 $httpClient.get(url, function(error, response, data){
